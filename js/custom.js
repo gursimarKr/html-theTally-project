a2165cdf602681f471a66cc0b2941d9616afddc9
aneeshsingh <aneesh@alkye.com>
2021-04-14 16:26:13 +0530

    profile page

--- js/custom.js
+++ js/custom.js
@@ -37,6 +37,41 @@ $(document).ready(function(){
     });
   });
 
+  $('.card-slider').slick({
+      speed: 500,
+      dots: false,
+      infinite: false,
+      slidesToShow: 3,
+      centerMode: false,
+      slidesToScroll: 2,
+      nextArrow: $('.slick_next.slick_next_post'),
+      prevArrow: $('.slick_previous.slick_previous_post'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
   $(".category_menu a").on('click', function(){
     var $id = $(this).attr('href');
     var $pos = $($id).offset().top - 90 
