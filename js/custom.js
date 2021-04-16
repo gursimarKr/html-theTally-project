f50bf3580139a8781692d6bfd54a248410571293
aneeshsingh <aneesh@alkye.com>
2021-04-16 09:51:49 +0530

    updated pages

--- js/custom.js
+++ js/custom.js
@@ -101,19 +101,35 @@ $(document).ready(function(){
   });
 
 
-  var myModalEl = document.getElementById('reviewModal')
-  myModalEl.addEventListener('shown.bs.modal', function (event) {
-    $('#textarea').on('keyup', function(event) {
-     var len = $(this).val().length;
-     var charVal = 500 - len;
-     if (len <= 500) {
-        $("#charNum").html(charVal);
-     }
+  $('.profile_detail input[type="radio"]').click(function() {
+    var inputValue = $(this).attr("value");
+    var targetBox = $("." + inputValue);
+    $(".profile_detail_box").not(targetBox).hide();
+    $(targetBox).show();
   });
-  })
 
-  $('.profile_detail .form-check-input').change(function(){
-    console.log($(this).val());
+  $count = 2;
+  $('body').on('click', '.add-child', function(e){
+    e.preventDefault();
+    $(this).parent().hide();
+    var $clone = `<div class="profile_detail_box yeskids" style="display: block;"><div class="mb-4 profile_detail_subheading border-bottom pb-2 text-uppercase">Child ${$count}</div><h4 class="mb-3">What is their date of birth?</h4><div class="row align-items-end"><div class="col-sm-6 col-md-4"><label class="form-label">Select Month</label><select class="form-select"><option selected>xxx</option><option value="1">xxx</option><option value="2">xxx</option><option value="3">xxx</option></select></div><div class="col-sm-6 col-md-4"><label class="form-label">Select Year</label><select class="form-select"><option selected>2000</option><option value="1">2001</option><option value="2">2002</option><option value="3">2003</option></select></div><div class="col-sm-6 col-md-4"><button class="btn add-child mb-0 w-100 btn-outline-blue">Add Another Child</button></div></div></div>`;
+      // alert($clone)
+    $('.profile_detail_box_childs').append($clone);
+
+    $count++;
   })
 
 });
+
+var myModalEl = document.getElementById('reviewModal');
+if(myModalEl){
+  myModalEl.addEventListener('shown.bs.modal', function (event) {
+      $('#textarea').on('keyup', function(event) {
+       var len = $(this).val().length;
+       var charVal = 500 - len;
+       if (len <= 500) {
+          $("#charNum").html(charVal);
+       }
+    });
+  })
+}
